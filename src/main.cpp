60f074f12dff306a4919e35af7378d15fd3b5e4d
Axodarap <jakob.soukup@aon.at>
2024-06-01 23:00:12 +0200

    bmp stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,56 @@
 #include <Arduino.h>
 #include "SoilSensors.h"
+#include "EnvironmentalSensor.h"
+
+#include <Adafruit_BME280.h>
+Adafruit_BME280 bme; // I2C
 
 SoilSensors soil_sensors(4, 5, 6, 7, A0);
+EnvironmentalSensor bmp_1(0x76);
+EnvironmentalSensor bmp_2(0x77);
 
 void setup() {
   Serial.begin(9600);
-  soil_sensors.Init();
+  if(bmp_1.Init()){
+    Serial.print("Sensor with address ");
+    Serial.print(bmp_1.GetAddress());
+    Serial.print(" succesfully connected.");
+  }
+  else{
+    Serial.print("Sensor with address ");
+        Serial.print(bmp_1.GetAddress());
+        Serial.println(" not found");
+  }
+  if(bmp_2.Init()){
+    Serial.print("Sensor with address ");
+    Serial.print(bmp_2.GetAddress());
+    Serial.print(" succesfully connected.");
+  }
+  else{
+    Serial.print("Sensor with address ");
+        Serial.print(bmp_2.GetAddress());
+        Serial.println(" not found");
+  } 
+
+  if (!bme.begin()) {  
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
 }
 
 void loop() {
-  
-for(int i=1; i<17; i++){
-  Serial.print((String)soil_sensors.ReadMoisture(i) + "\t");  
- }
- Serial.println();
- delay(500);
+/*   Serial.print("Humidity Sensor 1:\t");
+  Serial.println(bmp_1.ReadHumidity());
+  Serial.print("Humidity Sensor 2: \t");
+  Serial.println(bmp_2.ReadHumidity());
+  Serial.print("Temperature Sensor 1: \t");
+  Serial.println(bmp_1.ReadHumidity());
+  Serial.print("Temperature Sensor 2: \t");
+  Serial.println(bmp_2.ReadHumidity());
+  Serial.print("Pressure Sensor 1: \t");
+  Serial.println(bmp_1.ReadPressure());
+  Serial.print("Pressure Sensor 2: \t");
+  Serial.println(bmp_2.ReadPressure());
+  Serial.println("----------------------------------------------");
+  delay(1000); */
 }
